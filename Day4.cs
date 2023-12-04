8cd5e92035e574217e189c7547e1c7b57ee23605
LudvigRisan <ludvig.risan@gmail.com>
2023-12-04 10:03:59 +0100

    Initial commit

---
+++ Day4.cs
@@ -0,0 +1,61 @@
+﻿namespace AdventOfCode2023;
+
+public class Day4
+{
+    
+    
+    public static void solve()
+    {
+        StreamReader reader = new StreamReader(@"..\..\..\..\Inputs\input_04.txt");
+        string? line = reader.ReadLine();
+
+        int sum = 0;
+
+        List<int> copies = new List<int>();
+        
+        while (line != null)
+        {
+            int copyValue = copies.Count > 0 ? copies[0] : 1;
+            if (copies.Count > 0)
+            {
+                copies.RemoveAt(0);
+            }
+            line = line.Split(':')[1];
+            string[] winning = line.Split('|')[0].Split(' ');
+            string[] numbers = line.Split('|')[1].Split(' ');
+
+            int points = 0;
+            
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                if(numbers[i] == " " || numbers[i] == "") continue;
+                // Console.WriteLine(numbers[i]);
+                if (winning.Contains(numbers[i]))
+                {
+                    points++;
+                }
+            }
+
+            for (int i = 0; i < points; i++)
+            {
+                if (copies.Count > i)
+                {
+                    copies[i] += copyValue;
+                }
+                else
+                {
+                    copies.Add(copyValue + 1);
+                }
+            }
+
+            sum += copyValue;
+            
+            line = reader.ReadLine();
+        }
+        
+        Console.WriteLine(sum);
+    }
+    
+    
+    
+}
---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿using System.IO;
+using AdventOfCode2023;
+
+// using System.Text.RegularExpressions;
+//
+// StreamReader reader = new StreamReader(@"..\..\..\..\Inputs\input_01.txt");
+//
+// string[] numbers = { "one", "two", "three", "four", "five", "six", "seven", "eight", "nine" };
+// string cleaner = "[^0-9]";
+// string? line;
+//
+// line = reader.ReadLine();
+// int sum = 0;
+//
+// while (line != null)
+// {
+//     for (int i = 0; i < numbers.Length; i++)
+//     {
+//         line = Regex.Replace(line, "(" +numbers[i] + ")",  "$&" + (i + 1).ToString() + "$&");
+//     }
+//     // Console.WriteLine(line);
+//
+//     line = Regex.Replace(line, cleaner, "");
+//     
+//     
+//     List<char> values = line.ToList();
+//
+//     sum += int.Parse(values[0].ToString() + values.Last().ToString());
+//     
+//     line = reader.ReadLine();
+// }
+//
+// Console.WriteLine(sum);
+
+Day4.solve();
+
