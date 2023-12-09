7fb952311e18df1de243f94a123aa96869f07ecc
Ludvig Risan <ludvig.risan@gmail.com>
2023-12-09 11:59:10 +0100

    COMPLETE!!!
    Finnished day 8, a complete mess

--- Day8.cs
+++ Day8.cs
@@ -15,7 +15,7 @@ public class Day8
         StreamReader reader = new StreamReader(@"..\..\..\..\Inputs\input_08.txt");
         string? line = reader.ReadLine();
         List<Node> nodes = new List<Node>();
-        long stepCounter = 0;
+        long stepCounter = -3;
         string sequence = line;
         reader.ReadLine();
         line = reader.ReadLine();
@@ -30,45 +30,67 @@ public class Day8
             line = reader.ReadLine();
         }
 
-        int sequenceIndex = 0;
+        int sequenceIndex = -3;
         List<Node> currentNodes = nodes.FindAll((x) => x.iD[2] == 'A');
         int[] lengths = { 19633, 17293, 12601, 23149, 13773, 20806 };
         int[] offsets = {3, 7, 3, 3, 3, 4};
-        // for(int i = 0; i < currentNodes.Count; i++)
-        // {
-        //     int offset = 0;
-        //     sequenceIndex = 0;
-        //     var search = new List<Node>();
-        //     while (!search.Contains(new Node{foundIndex = sequenceIndex, iD = currentNodes[i].iD}))
-        //     {
-        //         search.Add(new Node{foundIndex = sequenceIndex, iD = currentNodes[i].iD});
-        //         currentNodes[i] = nodes.Find(x => x.iD == (sequence[sequenceIndex] == 'L' ? currentNodes[i].l : currentNodes[i].r));
-        //         sequenceIndex = (sequenceIndex + 1) % sequence.Length;
-        //         if (offset > 0 || currentNodes[i].iD[2] == 'Z')
-        //         {
-        //             offset++;
-        //         }
-        //         
-        //         // Console.Write(currentNodes[i].iD[2] == 'Z' ? "-" : "");
-        //     }
-        //     Console.WriteLine(search.Count + " : " + offset);
-        //     lengths.Add(search.Count());
-        //     offsets.Add(offset);
-        //     // Console.WriteLine()
-        // }
+        for(int i = 0; i < currentNodes.Count; i++)
+        {
+            int offset = 0;
+            sequenceIndex = 0;
+            var search = new List<Node>();
+            // while (!search.Contains(new Node{foundIndex = sequenceIndex, iD = currentNodes[i].iD}))
+            Console.WriteLine(currentNodes[i].iD + ": " + currentNodes[i].l + " | " + currentNodes[i].r + " | " + sequence[sequenceIndex]);
+            while (currentNodes[i].iD[2] != 'Z'){
+                offset++;
+                // search.Add(new Node{foundIndex = sequenceIndex, iD = currentNodes[i].iD});
+                currentNodes[i] = nodes.Find(x => x.iD == (sequence[sequenceIndex] == 'L' ? currentNodes[i].l : currentNodes[i].r));
+                sequenceIndex = (sequenceIndex + 1) % sequence.Length;
+                // if (offset > 0 || currentNodes[i].iD[2] == 'Z')
+                // {
+                //     offset++;
+                // }
+                
+                // Console.Write(currentNodes[i].iD[2] == 'Z' ? "-" : "");
+            }
+            // Console.WriteLine(search.Count + " : " + offset);
+            Console.WriteLine(currentNodes[i].iD + ": " + currentNodes[i].l + " | " + currentNodes[i].r + " | " + sequence[sequenceIndex]);
+            Console.WriteLine(offset);
+            //!IMPORTANT!!!! offset values input into least common multiple calc, it's that easy
+            
+            // Console.WriteLine()
+        }
         
 
-        bool found = false;
-        int lengthCount = lengths.Length;
-        while (!found)
-        {
-            stepCounter+=(17293 * 12601);
-            found = true;
-            for (int i = 0; i < lengthCount; i++)
-            {
-                found = found && ((stepCounter + offsets[i]) % lengths[i] == 0);
-            }
-        }
+        // bool found = false;
+        // int lengthCount = lengths.Length;
+        // while (!found)
+        // {
+        //     stepCounter+=(247395433);
+        //     found = true;
+        //     // List<int> foundList = new List<int>();
+        //
+        //     // if ((stepCounter + offsets[1]) % lengths[1] == 0){
+        //     //     found = true;
+        //     // }
+        //     
+        //     for (int i = 0; i < lengthCount; i++)
+        //     {
+        //         // if ((stepCounter + offsets[i]) % lengths[i] == 0){
+        //         //     foundList.Add(i);
+        //         // }
+        //         found = found && ((stepCounter + offsets[i]) % lengths[i] == 0);
+        //     }
+        //
+        //     // if (foundList.Count > 3){
+        //     //     Console.Write(stepCounter + " : " );
+        //     //
+        //     //     for (int i = 0; i < foundList.Count; i++){
+        //     //         Console.Write(foundList[i]);
+        //     //     }
+        //     //     Console.WriteLine();
+        //     // }
+        // }
         
         // Console.WriteLine();
         // while (currentNodes.Any(x => x.iD[2] != 'Z'))
